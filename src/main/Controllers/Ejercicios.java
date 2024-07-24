5a37b8b98ec690a03dd8c210a82e313e044f9b84
fernando <fecholkm19@gmail.com>
2024-07-24 17:53:36 -0500

    Examen Final :(

---
+++ src/main/Controllers/Ejercicios.java
@@ -0,0 +1,13 @@
+package main.Controllers;
+
+public class Ejercicios {
+    public void contarFrecuenciaDePalabras (String text){
+
+    }
+
+
+    public void verificarCaracteresUnicos(String input){
+        
+    }
+    
+}
---
+++ src/main/Controllers/EstudianteController.java
@@ -0,0 +1,20 @@
+package main.Controllers;
+
+import java.util.HashMap;
+
+import main.Models.Estudiante;
+
+public class EstudianteController {
+    String obtenerCategoria;
+
+
+    public HashMap procesarEstudiantes (Estudiante[] estudiantes){
+        
+
+
+
+
+        return null;
+    }
+    
+}
